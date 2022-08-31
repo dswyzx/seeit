aa86891b4e3f0fa133953f62dc589b2b5a3e91c4
dswyzx <dswyzx@live.com>
2022-08-31 16:07:28 +0800

    first

---
+++ docs/here.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector('#seeit');
+
+
+btn.addEventListener('click',() => {
+	const input = document.querySelector('input');
+ 
+ 	input.setAttribute('value', window.atob('bmt5dHMuc2l0ZQ=='));
+	 
+	 
+});
